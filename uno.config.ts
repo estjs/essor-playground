c25d25c2476bf545d96a205ce207eb129c11c082
jiangxd <jiangxd2016@gmail.com>
2024-05-19 15:41:08 +0800

    fix: fix border-color

--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig, presetIcons, presetUno } from 'unocss';
 export default defineConfig({
   theme: {},
+  shortcuts: {
+    'b-base': 'border-#aaa5',
+  },
   presets: [
     presetUno(),
     presetIcons({
       scale: 1.2,
+      warn: true,
     }),
   ],
 });
