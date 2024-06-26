caf228ff097a3f3b36b92ae1d5b56f90d337c32a
jiangxd <jiangxd2016@gmail.com>
2024-06-26 14:53:45 +0800

    chore: update essor verison

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const importMapCOnfig = {
-  essor: 'https://cdn.jsdelivr.net/npm/essor@0.0.6-beta.13/dist/essor.esm.js',
+  essor: 'https://cdn.jsdelivr.net/npm/essor@0.0.6-beta.14/dist/essor.esm.js',
 };
